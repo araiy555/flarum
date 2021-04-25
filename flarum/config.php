c0eb55af4610c95d4a1db65c8941fda5b5d1ce5d
新井耀介 <k016c1200@it-neec.jp>
2021-04-25 23:23:50 +0900

    commit

---
+++ flarum/config.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'debug' => false,
+  'poweredByHeader' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => 'q1w2e3r4',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://192.168.33.10',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
