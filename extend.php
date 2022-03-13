a7a9da6dea5d1a507a26531f05298949c6428581
新井耀介 <k016c1200@it-neec.jp>
2022-03-13 20:15:38 +0900

    commit

--- extend.php
+++ extend.php
@@ -28,7 +28,9 @@ return [
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-<script data-ad-client="ca-pub-3518678681279293" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>           
+<script data-ad-client="ca-pub-3518678681279293" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3518678681279293"
+     crossorigin="anonymous"></script>
 <script src="/js/main.js"></script>           
 <link rel="stylesheet" href="/css/main.css">
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
