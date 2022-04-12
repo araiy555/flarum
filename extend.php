6f25b8d538495d5e054f049ce716493cb7d745f6
新井耀介 <k016c1200@it-neec.jp>
2022-04-13 00:00:47 +0900

    commit

--- extend.php
+++ extend.php
@@ -17,6 +17,12 @@ return [
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3518678681279293"
      crossorigin="anonymous"></script>
 <meta name="google-site-verification" content="k5wKwgbijqTSvyCmvRt_VhtBaGcF9DYuhZcAz5F1-DI" />
+<script type="text/javascript">
+    window._mNHandle = window._mNHandle || {};
+    window._mNHandle.queue = window._mNHandle.queue || [];
+    medianet_versionId = "3121199";
+</script>
+<script src="https://contextual.media.net/dmedianet.js?cid=8CULH3OQH" async="async"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-0MBHKP37RC"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
